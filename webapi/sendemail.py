73eae4a1ad81117833462156e12cf4aadfb92fa5
Marcelo Cutulo <marcelo.cutulo@gmail.com>
2022-04-03 18:29:10 -0300

    Ajuste para envio a un destinatário

--- webapi/sendemail.py
+++ webapi/sendemail.py
@@ -6,9 +6,10 @@ from sendgrid.helpers.mail import Mail
 
 message = Mail(
     from_email='no-reply@mkv.tec.br',
-    to_emails=['marcelo.cutulo82@gmail.com', 'marcelo.cutulo@gmail.com', 'marcelo.cutulo@iob.com.br'], 
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    to_emails='marcelo.cutulo82@gmail.com', 
+    # to_emails=['marcelo.cutulo82@gmail.com', 'marcelo.cutulo@gmail.com', 'marcelo.cutulo@iob.com.br'], 
+    subject='Enviando E-mail para testar o SendGrid',
+    html_content='<strong>Teste de envio utilizando API do Sendgrid com Python</strong>')
 try:
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
